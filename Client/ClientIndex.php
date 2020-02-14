9762cfab6856c414facef8bdd6903fcc1df505fa
Damian <reacher93@gmail.com>
2020-02-14 16:55:40 +0100

    SQL Table to Array added

--- Client/ClientIndex.php
+++ Client/ClientIndex.php
@@ -4,12 +4,14 @@
 <?php
 
 //php Variablen
-$dbservername = "localhost";
-$dbname = dbname;
-$dbuser = 'username';
-$dbpw = 'password';
+$dbservername = "192.168.1.113";
+//$dbname = RoomMonitoring;
+$dbuser = 'mysqladmin';
+$dbpw = 'zbwzbw';
 
 $id = 0;
+$date = '00.00.0000';
+$time = 0;
 $temp = 0.0;
 $humidity = 0;
 $airPressure = 0;
@@ -17,24 +19,37 @@ $sonic = 0;
 $meterAboveSea = 0;
 $brightness = 0.0;
 
-//Array initialisieren
-$sensorData = array(
-    array($temp, $humidity, $airPressure, $sonic, $meterAboveSea, $brightness)
+/*Array initialisieren
+Node 1 (
+    array(Datum, Zeit, Temp, Luftfeuchtigkeit, Druck, Schallpegel, Meter über Meer, Helligkeit)
+    array(Datum, Zeit, Temp, Luftfeuchtigkeit, Druck, Schallpegel, Meter über Meer, Helligkeit)
 )
 
+*/
+
+$sensorData1 = array(
+    array()
+);
+
 //Verbindungsaufbau zur DB über PDO (dbname, user und pw von)
-try{
-    $pdo = new PDO('mysql:host=$dbservername;dbname=$dbname', $dbuser, $dbpw);
-    
+try {
+    $pdo = new PDO('mysql:host=192.168.1.113;dbname=RoomMonitoring', $dbuser, $dbpw);
+
     // den PDO-Fehlermodus auf Ausnahme setzen
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully to DB";
-    }
-catch(PDOException $e)
-    {
+
+    $sqlOrder = $pdo->prepare("SELECT * FROM Node1");
+    $sqlOrder->execute();
+
+    $sensorData1 = $sqlOrder->fetchAll();
+} catch (PDOException $e) {
     echo "Connection to DB failed: " . $e->getMessage();
 }
 
+
+
+print_r($sensorData1);
 
 ?>
 
